7f21d667cdfdb5eb4ec601b510ad6a9afe4f79c6
Kamalesh <ramkamalesh2004@gmail.com>
2023-03-04 13:34:31 +0530

    4.3.2

--- DSALab/Lab4/dsa43.c
+++ DSALab/Lab4/dsa43.c
@@ -1,44 +1,60 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int findZeroes(int a) {
-  if (a == 0)
-    return 0;
-  return a / 5 + findZeroes(a / 5);
-}
+int trailing_zeros(int n) {
+  if (n < 0)
+    return -1;
 
-int countPairs(int arr[], int n) {
-  int max = arr[0], count = 0;
-  for (int i = 0; i < n; i++) {
-    if (max < arr[i])
-      max = arr[i];
-  }
+  int count = 0;
 
-  int trailingzeroes[n];
-  for (int i = 0; i < n; i++) {
-    trailingzeroes[i] = findZeroes(arr[i]);
-  }
-
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = i + 1; j < n; j++) {
-      if (trailingzeroes[i] == trailingzeroes[j]) {
-        count++;
-      }
-    }
-  }
+  for (int i = 5; n / i >= 1; i *= 5)
+    count += n / i;
 
   return count;
 }
+
+int max(int num1, int num2) { return (num1 > num2) ? num1 : num2; }
 
 int main() {
   int n;
   scanf("%d", &n);
 
-  int list[n];
-  for (int i = 0; i < n; i++)
-    scanf("%d", &list[i]);
+  int arr[n];
 
-  int pairs = countPairs(list, n);
+  int m = 0;
 
-  printf("%d", pairs);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+    m = max(m, arr[i]);
+  }
+
+  int maxzero = trailing_zeros(m);
+
+  if (m == 1 || maxzero == 0) {
+    maxzero = n;
+  }
+
+  int zero[maxzero];
+
+  for (int i = 0; i < maxzero; i++) {
+    zero[i] = 0;
+  }
+
+  for (int i = 0; i < n; i++) {
+    zero[trailing_zeros(arr[i])]++;
+  }
+
+  int ans = 0;
+  for (int i = 0; i < maxzero; i++) {
+    if (zero[i] == 2)
+      ans++;
+    else if (zero[i] > 2) {
+      int temp = zero[i];
+      ans = ans + (temp) * (temp - 1) / 2;
+    } else {
+      continue;
+    }
+  }
+
+  printf("%d", ans);
+  return 0;
 }
